2457cb52c97cc02b16366e312b1bfa49d1d66b16
reostan <reostan32325@gmail.com>
2020-07-14 22:50:07 +0545

    New codes added for reverse sum13 process

---
+++ practice.py
@@ -0,0 +1,16 @@
+def sum13(nums):
+    s = 0
+    if len(nums) > 0 and nums[-1] != 13:
+        s = nums[-1]
+    for i in range(len(nums)-2, -1, -1):
+        if nums[i] != 13 and nums[i + 1] != 13:
+            s += nums[i]
+
+    print(s)
+
+
+sum13([30, 2, 4, 6, 30, 13, 8, 9])
+
+
+
+
